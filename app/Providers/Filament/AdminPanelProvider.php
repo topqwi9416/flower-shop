c0e0742b793cd1fda57e169a619a1a93faddfb1e
Mikhail <vladabdyl0@gmail.com>
2026-07-02 01:17:28 +0300

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -58,6 +58,7 @@ class AdminPanelProvider extends PanelProvider
             ->renderHook(
                 'panels::head.start',
                 fn () => '<script src="https://cdn.tailwindcss.com"></script>'
+                
             );   
     }
 }
