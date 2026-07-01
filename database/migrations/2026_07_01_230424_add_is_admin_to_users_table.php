334864e747c4324ed365e438c15596aba7e4d162
Mikhail <vladabdyl0@gmail.com>
2026-07-02 02:19:49 +0300

    Update 2026_07_01_230424_add_is_admin_to_users_table.php

--- database/migrations/2026_07_01_230424_add_is_admin_to_users_table.php
+++ database/migrations/2026_07_01_230424_add_is_admin_to_users_table.php
@@ -8,19 +8,19 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            if (!Schema::hasColumn('users', 'is_admin')) {
+        if (!Schema::hasColumn('users', 'is_admin')) {
+            Schema::table('users', function (Blueprint $table) {
                 $table->boolean('is_admin')->default(false);
-            }
-        });
+            });
+        }
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            if (Schema::hasColumn('users', 'is_admin')) {
+        if (Schema::hasColumn('users', 'is_admin')) {
+            Schema::table('users', function (Blueprint $table) {
                 $table->dropColumn('is_admin');
-            }
-        });
+            });
+        }
     }
 };
