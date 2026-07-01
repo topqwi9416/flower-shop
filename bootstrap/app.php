3603c4feecdaf135f20d952a761e7599a47b6e57
Mikhail <vladabdyl0@gmail.com>
2026-07-02 01:45:40 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Доверяем всем прокси (Render, Cloudflare и т.д.)
         $middleware->trustProxies(at: '*');
+        
+        // Убедись, что сессии включены
+        $middleware->web(append: [
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
