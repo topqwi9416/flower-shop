74b29883f69b1722cccad909aadc134a597dca5c
Mikhail <vladabdyl0@gmail.com>
2026-07-02 01:34:21 +0300

    Update app.php
    Added middleware to trust all proxies in app configuration.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Доверяем всем прокси (Render, Cloudflare и т.д.)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
