da6a6ef78204eb4539a92d1246c47c7bb0aed08e
Mikhail <vladabdyl0@gmail.com>
2026-07-02 02:11:16 +0300

    fix migration to check column existence

--- database/migrations/2026_07_01_230424_add_is_admin_to_users_table.php
+++ database/migrations/2026_07_01_230424_add_is_admin_to_users_table.php
@@ -9,14 +9,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin')->default(false);
+            if (!Schema::hasColumn('users', 'is_admin')) {
+                $table->boolean('is_admin')->default(false);
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('is_admin');
+            if (Schema::hasColumn('users', 'is_admin')) {
+                $table->dropColumn('is_admin');
+            }
         });
     }
 };
