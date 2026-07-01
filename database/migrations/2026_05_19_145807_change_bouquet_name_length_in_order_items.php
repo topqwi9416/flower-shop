f4ea150fbc626d597bb286f57d6c2fb49c1c774b
Mikhail <vladabdyl0@gmail.com>
2026-07-02 02:16:29 +0300

    Update 2026_05_19_145807_change_bouquet_name_length_in_order_items.php

--- database/migrations/2026_05_19_145807_change_bouquet_name_length_in_order_items.php
+++ database/migrations/2026_05_19_145807_change_bouquet_name_length_in_order_items.php
@@ -1,27 +1,17 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
-    public function up()
+return new class extends Migration
+{
+    public function up(): void
     {
-        Schema::table('order_items', function (Blueprint $table) {
-            $table->string('bouquet_name', 1000)->change();
-        });
+        DB::statement('ALTER TABLE order_items ALTER COLUMN bouquet_name TYPE VARCHAR(1000)');
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::table('order_items', function (Blueprint $table) {
-            //
-        });
+        DB::statement('ALTER TABLE order_items ALTER COLUMN bouquet_name TYPE VARCHAR(255)');
     }
 };
